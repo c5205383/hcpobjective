0ca2311111821bc4c9234ed3826b582dec98b2a2
Zero Yu <zero.yu@sap.com>
2016-03-25 06:24:45 +0000

    description

--- webapp/controller/EmptyPage.controller.js
+++ webapp/controller/EmptyPage.controller.js
@@ -6,6 +6,7 @@ sap.ui.define([
 	return Controller.extend("hcpobjective.controller.EmptyPage", {
 		// Handler for the nav button of the page. It is attached declaratively. Note that it is only available on phone.
 		onNavButtonPress: function() {
+			alert("==");
 			var oApplicationProperties = this.getView().getModel("appProperties");
 			var oApplicationController = oApplicationProperties.getProperty("/applicationController");
 			oApplicationController.navBack(true);
